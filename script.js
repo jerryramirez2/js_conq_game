5709c3b5c2d032a7f2d3657fd78c966a80832048
jerryramirez2 <jr7245486@gmail.com>
2026-07-06 15:53:39 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+
+//HTML 
+let opponentContainer = document.getElementById("opponentContainer");
+let playerContainer = document.getElementById("playerContainer");
+
+window.onload = () => {
+    createCards();
+}
+
+function createCards() {
+
+    let type = ['S', 'C', 'D', 'H'];
+    let values = ['A', '2', '3', '4', '5', '6', '7', '10', 'Q', 'K'];
+
+    for (let i = 0; i < type.length; i++) {
+        for (let j = 0; j < values.length; j++) {
+            
+        }
+    }
+}
